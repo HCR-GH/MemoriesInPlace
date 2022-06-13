eb92880a474cc3eb4bd77c31365ad954f686d269
HCR-GH <hattierowling@gmail.com>
2022-06-13 13:33:46 +0100

    recording start / finish messages added

--- scripts/recordAudio.js
+++ scripts/recordAudio.js
@@ -15,7 +15,7 @@ navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
     var url = URL.createObjectURL(blob);
     audio.src = url;
     var para = document.createElement("p")
-    para.innerText = "recording finished 2.1"
+    para.innerText = "Recording Finished"
     document.body.appendChild(para)
     downloadLink.href = URL.createObjectURL(blob)
     downloadLink.download = test2.wav
@@ -24,6 +24,9 @@ navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
 
   document.getElementById('record').addEventListener('click', function () {
     mediaRecorder.start();
+    var para = document.createElement("p")
+    para.innerText = "Recording Started"
+    document.body.appendChild(para)
   });
 
   document.getElementById('stop').addEventListener('click', function () {
